e93e680ac82a58736e50bdba417ed8e5e1527b1e
Nikhil Anand <anandn@oregonstate.edu>
2018-08-27 14:47:39 -0700

    Added Outs and other buttons

--- app/src/main/java/com/example/android/crickit/InitiateLogin.java
+++ app/src/main/java/com/example/android/crickit/InitiateLogin.java
@@ -1,7 +1,10 @@
 package com.example.android.crickit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InitiateLogin extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class InitiateLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initiate_login);
+
+        configureNextButton();
+    }
+
+    private void configureNextButton(){
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(InitiateLogin.this,MainActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/android/crickit/MainActivity.java
+++ app/src/main/java/com/example/android/crickit/MainActivity.java
@@ -18,14 +18,19 @@ public class MainActivity extends AppCompatActivity {
     TextView team_b_score;
     TextView ball_numb;
     TextView over_numb;
+    TextView outnumb;
     //EditText team_a_score;
-    Button   btnfour, btnsix, btnwide, btnreset;
+    Button   btnfour, btnsix, btnwide, btnreset, btnone,btntwo,btnthree,btnfive,btnnoball,btnout;
     ToggleButton btnwhobats;
     ImageView team_a_bat_image, team_b_bat_image, blackballone,blackballtwo,blackballthree,blackballfour,blackballfive,blackballsix;
     int Ascore = 0;
     int Bscore = 0;
     int balls = 0;
+    int aouts = 0;
+    int bouts = 0;
+    float newballs = 0;
     int overs = 0;
+    float newovers = 0;
     boolean isTeamABatting = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +39,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         team_a_score =  findViewById(R.id.team_a_score);
         team_b_score =  findViewById(R.id.team_b_score);
+        outnumb = findViewById(R.id.outnumb);
         ball_numb = findViewById(R.id.ball_numb);
         over_numb = findViewById(R.id.over_numb);
         btnfour = findViewById(R.id.btnfour);
         btnsix = findViewById(R.id.btnsix);
+        btnone = findViewById(R.id.btnone);
+        btntwo = findViewById(R.id.btntwo);
+        btnthree = findViewById(R.id.btnthree);
+        btnfive = findViewById(R.id.btnfive);
+        btnnoball = findViewById(R.id.btnnoball);
         btnwide = findViewById(R.id.btnwide);
         btnwide = findViewById(R.id.btnwide);
         btnreset = findViewById(R.id.btnreset);
+        btnout = findViewById(R.id.btnout);
         btnwhobats = findViewById(R.id.btnwhobats);
         team_a_bat_image = findViewById(R.id.team_a_bat_image);
         team_b_bat_image = findViewById(R.id.team_b_bat_image);
@@ -81,6 +93,9 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if(balls != 5){
                     balls+=1;
+                    newballs +=0.1;
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
                     ball_numb.setText(balls+"");
                     if(balls == 1){
                         blackballone.setImageResource(R.drawable.rball);
@@ -102,9 +117,11 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
                     balls = 0;
+                    newballs = 0;
                     ball_numb.setText(balls+"");
                     overs +=1;
-                    over_numb.setText(overs+"");
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
                     blackballone.setImageResource(R.drawable.bball);
                     blackballtwo.setImageResource(R.drawable.bball);
                     blackballthree.setImageResource(R.drawable.bball);
@@ -128,6 +145,9 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if(balls != 5){
                     balls+=1;
+                    newballs +=0.1;
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
                     ball_numb.setText(balls+"");
                     if(balls == 1){
                         blackballone.setImageResource(R.drawable.rball);
@@ -147,9 +167,11 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
                     balls = 0;
+                    newballs = 0;
                     ball_numb.setText(balls+"");
                     overs +=1;
-                    over_numb.setText(overs+"");
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
                     blackballone.setImageResource(R.drawable.bball);
                     blackballtwo.setImageResource(R.drawable.bball);
                     blackballthree.setImageResource(R.drawable.bball);
@@ -186,7 +208,222 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        //New Code for runs
+        btnone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isTeamABatting) {
+                    Ascore += 1;
+                    team_a_score.setText(Ascore+"");
+                }
+                else {
+                    Bscore += 1;
+                    team_b_score.setText(Bscore + "");
+                }
+                if(balls != 5){
+                    balls+=1;
+                    newballs +=0.1;
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
+                    ball_numb.setText(balls+"");
+                    if(balls == 1){
+                        blackballone.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 2){
+                        blackballtwo.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 3){
+                        blackballthree.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 4){
+                        blackballfour.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 5){
+                        blackballfive.setImageResource(R.drawable.rball);
+                    }
+                }
+                else{
+                    balls = 0;
+                    newballs = 0;
+                    ball_numb.setText(balls+"");
+                    overs +=1;
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
+                    blackballone.setImageResource(R.drawable.bball);
+                    blackballtwo.setImageResource(R.drawable.bball);
+                    blackballthree.setImageResource(R.drawable.bball);
+                    blackballfour.setImageResource(R.drawable.bball);
+                    blackballfive.setImageResource(R.drawable.bball);
+                    blackballsix.setImageResource(R.drawable.bball);
+                    //changes ball color
+                }
+            }
+        });
+        btntwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isTeamABatting) {
+                    Ascore += 2;
+                    team_a_score.setText(Ascore+"");
+                }
+                else {
+                    Bscore += 2;
+                    team_b_score.setText(Bscore + "");
+                }
+                if(balls != 5){
+                    balls+=1;
+                    newballs +=0.1;
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
+                    ball_numb.setText(balls+"");
+                    if(balls == 1){
+                        blackballone.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 2){
+                        blackballtwo.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 3){
+                        blackballthree.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 4){
+                        blackballfour.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 5){
+                        blackballfive.setImageResource(R.drawable.rball);
+                    }
+                }
+                else{
+                    balls = 0;
+                    newballs = 0;
+                    ball_numb.setText(balls+"");
+                    overs +=1;
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
+                    blackballone.setImageResource(R.drawable.bball);
+                    blackballtwo.setImageResource(R.drawable.bball);
+                    blackballthree.setImageResource(R.drawable.bball);
+                    blackballfour.setImageResource(R.drawable.bball);
+                    blackballfive.setImageResource(R.drawable.bball);
+                    blackballsix.setImageResource(R.drawable.bball);
+                    //changes ball color
+                }
+            }
+        });
 
+        btnthree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isTeamABatting) {
+                    Ascore += 3;
+                    team_a_score.setText(Ascore+"");
+                }
+                else {
+                    Bscore += 3;
+                    team_b_score.setText(Bscore + "");
+                }
+                if(balls != 5){
+                    balls+=1;
+                    newballs +=0.1;
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
+                    ball_numb.setText(balls+"");
+
+                    if(balls == 1){
+                        blackballone.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 2){
+                        blackballtwo.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 3){
+                        blackballthree.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 4){
+                        blackballfour.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 5){
+                        blackballfive.setImageResource(R.drawable.rball);
+                    }
+                }
+                else{
+                    balls = 0;
+                    newballs = 0;
+                    ball_numb.setText(balls+"");
+                    overs +=1;
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
+                    blackballone.setImageResource(R.drawable.bball);
+                    blackballtwo.setImageResource(R.drawable.bball);
+                    blackballthree.setImageResource(R.drawable.bball);
+                    blackballfour.setImageResource(R.drawable.bball);
+                    blackballfive.setImageResource(R.drawable.bball);
+                    blackballsix.setImageResource(R.drawable.bball);
+                    //changes ball color
+                }
+            }
+        });
+        btnfive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isTeamABatting) {
+                    Ascore += 5;
+                    team_a_score.setText(Ascore+"");
+                }
+                else {
+                    Bscore += 5;
+                    team_b_score.setText(Bscore + "");
+                }
+                if(balls != 5){
+                    balls+=1;
+                    newballs +=0.1;
+                    newovers = overs + newballs;
+                    ball_numb.setText(balls+"");
+                    over_numb.setText(newovers+"");
+                    if(balls == 1){
+                        blackballone.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 2){
+                        blackballtwo.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 3){
+                        blackballthree.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 4){
+                        blackballfour.setImageResource(R.drawable.rball);
+                    }
+                    if(balls == 5){
+                        blackballfive.setImageResource(R.drawable.rball);
+                    }
+                }
+                else{
+                    balls = 0;
+                    newballs = 0;
+                    ball_numb.setText(balls+"");
+                    overs +=1;
+                    newovers = overs + newballs;
+                    over_numb.setText(newovers+"");
+                    blackballone.setImageResource(R.drawable.bball);
+                    blackballtwo.setImageResource(R.drawable.bball);
+                    blackballthree.setImageResource(R.drawable.bball);
+                    blackballfour.setImageResource(R.drawable.bball);
+                    blackballfive.setImageResource(R.drawable.bball);
+                    blackballsix.setImageResource(R.drawable.bball);
+                    //changes ball color
+                }
+            }
+        });
+        btnout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isTeamABatting) {
+                    aouts += 1;
+                    outnumb.setText(aouts+"");
+                }
+                else {
+                    bouts += 1;
+                    outnumb.setText(bouts + "");
+                }
+            }
+        });
 
     }
 }
