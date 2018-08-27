e235dcd6a2cfdf777f52517a085eceb156d0c06c
Chinmay <wadgaonc@oregonstate.edu>
2018-08-27 04:50:44 -0700

    Added new activity, layout file and JAVA file. Still need to link using Intents

---
+++ app/src/main/java/com/example/android/crickit/InitiateLogin.java
@@ -0,0 +1,13 @@
+package com.example.android.crickit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitiateLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initiate_login);
+    }
+}
